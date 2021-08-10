a3a22f59e95974a34d60373bb2330dc1964e5103
Beksultan <59771095+Beka23@users.noreply.github.com>
2021-08-10 14:00:07 -0400

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Header from './Header'
+import Header from './Header';
 import ItemContainer from './ItemContainer'
 import ItemDetails from './ItemDetails'
 import Login from './Login'
